da378b35dcad48fc4ff2bf88a909e7048fe06337
Will Leinweber <will@bitfission.com>
2022-05-03 16:22:20 -0500

    cb 1.3.0
    chore: fix brew style
    
    chore: bump linux crystal version
    
    chore: maybe linux needs this
    
    chore: maybe this is not true anymore
    
    chore: try removing more things
    
    chore: try removing linux again
    
    Closes #8.
    
    Signed-off-by: Will Leinweber <1973+will@users.noreply.github.com>

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -1,57 +1,32 @@
 class Cb < Formula
   desc "CLI for Crunchy Bridge"
   homepage "https://crunchybridge.com"
-  url "https://github.com/CrunchyData/bridge-cli/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "be49a8e2a8b8b617e7049462ddfddc1a83c9eee5cb819b7f87ff2f42e24dbfbd"
+  url "https://github.com/CrunchyData/bridge-cli/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "9abda6a167ab46a93e0ac5724465f1793b56d21c5269074d058d1b2987b03544"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/CrunchyData/homebrew-brew/releases/download/cb-1.2.0"
     sha256 cellar: :any,                     arm64_big_sur: "ddd902b33a66f42052f4144a4f93c64e8cdd342f62eaa6f02552a637f9a9bc1d"
     sha256 cellar: :any,                     big_sur:       "9c02c7bdabf242e919e9bc2b55e0aa97fb0d2dd51143e78a2880c002735fc42a"
-    sha256 cellar: :any_skip_relocation,     x86_64_linux:  "743a64bfb7220258b449748b19db9615efefa8507a76100cfb8863f72bf188b8"
   end
 
   head do
     url "https://github.com/will/cb.git"
   end
 
+  depends_on "crystal" => :build
   depends_on "make" => :build
   depends_on "pkg-config" => :build
-
+  depends_on "bdw-gc"
   depends_on "libevent"
+  depends_on "libssh2"
   depends_on "openssl@1.1"
   depends_on "pcre"
 
   uses_from_macos "zlib"
 
-  on_macos do
-    depends_on "bdw-gc"
-    depends_on "crystal" => :build
-  end
-
-  resource "crystal" do
-    on_linux do
-      url "https://github.com/crystal-lang/crystal/releases/download/1.0.0/crystal-1.0.0-1-linux-x86_64.tar.gz"
-      version "1.0.0-1"
-      sha256 "00211ca77758e99210ec40b8c5517b086d2ff9909e089400f6d847a95e5689a4"
-    end
-  end
-
   def install
-    unless OS.mac?
-      # Use static Crystal compiler, since the one in Homebrew seems to be broken
-      # for Linux
-      (buildpath / "crystal").install resource("crystal")
-      ENV.prepend_path "PATH", "crystal/bin"
-      ENV.prepend_path "PATH", "crystal/embedded/bin"
-
-      ENV.prepend_path "PKG_CONFIG_PATH", (Formula["readline"].opt_lib / "pkgconfig")
-      ENV.prepend_path "PKG_CONFIG_PATH", (Formula["zlib"].opt_lib / "pkgconfig")
-
-      ENV.prepend_path "CRYSTAL_LIBRARY_PATH", (buildpath / "crystal/lib/crystal/lib")
-    end
-    # system "shards", "build", "--release"
     system "make", "build", "RELEASE=1"
 
     bin.install "bin/cb"
