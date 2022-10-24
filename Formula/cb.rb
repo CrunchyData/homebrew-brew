e3dc65d6ad8a5fd495dd3ec42fdc3c0b6e3b4430
Adam Brightwell <adam.brightwell@gmail.com>
2022-10-24 22:57:37 +0000

    cb: update 3.0.2 bottle.

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -6,9 +6,8 @@ class Cb < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    root_url "https://github.com/CrunchyData/homebrew-brew/releases/download/cb-3.0.1"
-    sha256 cellar: :any, arm64_big_sur: "ad996e7130a9bc7eb6fe1bec1a4e6229e67d5aa96019dca0e6444f742004a3ed"
-    sha256 cellar: :any, big_sur:       "45a1415d5e953fcc57a422c97b014906f2830aed0ebb8d80e7963e2ecae9c244"
+    root_url "https://github.com/CrunchyData/homebrew-brew/releases/download/cb-3.0.2"
+    sha256 cellar: :any, big_sur: "992473779b639f99a104d9fb206a476d3ec48c8c6a0f2fe3bd8c0f6d54b7d46d"
   end
 
   head do
