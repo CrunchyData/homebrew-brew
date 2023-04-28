25f1c4f19542e448035e231c1672043c706f1d81
admin <adam.brightwell@crunchydata.com>
2023-04-28 10:53:08 -0400

    fix arm mac

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -7,7 +7,8 @@ class Cb < Formula
 
   bottle do
     root_url "https://github.com/CrunchyData/homebrew-brew/releases/download/cb-3.3.0"
-    sha256 cellar: :any, monterey: "e38044b01b86ea3cac6a29474c11e7edd765fcbbecc1e35489f9c9061b244b39"
+    sha256 cellar: :any, arm64_monterey: "7c124f659e4448117ccc17e8bbc6117e2a3be82fc926aa23e91c936fa65ba2d1"
+    sha256 cellar: :any, monterey:       "e38044b01b86ea3cac6a29474c11e7edd765fcbbecc1e35489f9c9061b244b39"
   end
 
   head do
