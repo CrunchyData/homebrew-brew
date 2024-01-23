1aab798c5342720aae666b880de363b240498c47
admin <adam.brightwell@crunchydata.com>
2024-01-23 08:44:49 -0500

    cb 3.4.4
    Closes #25.
    
    Signed-off-by: Adam Brightwell <adam.brightwell@gmail.com>

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -1,18 +1,16 @@
 class Cb < Formula
   desc "CLI for Crunchy Bridge"
   homepage "https://crunchybridge.com"
-  url "https://github.com/CrunchyData/bridge-cli/archive/refs/tags/v3.4.3.tar.gz"
-  sha256 "6ae15ad6d6fa394e4ca1f4d119171a99e4b5e4dc6f5f07995c376d5bc6c81bee"
+  url "https://github.com/CrunchyData/bridge-cli/archive/refs/tags/v3.4.4.tar.gz"
+  sha256 "5c3244023d5d3a4c380d080f488313446ab73a14649e62eaadec353f4a4591d4"
   license "GPL-3.0-or-later"
+
+  head "https://github.com/crunchydata/bridge-cli.git", branch: "main"
 
   bottle do
     root_url "https://github.com/CrunchyData/homebrew-brew/releases/download/cb-3.4.3"
     sha256 cellar: :any, arm64_monterey: "fa8f5183088a7cbb451505e99a70c068c52cee39c06d239433770e9f910beb2b"
     sha256 cellar: :any, monterey:       "ee62aa10c87b1f7aa5be79999701e9562ac870bf158a5c7bc9d7ed4327b00d45"
-  end
-
-  head do
-    url "https://github.com/crunchydata/bridge-cli.git"
   end
 
   depends_on "crystal" => :build
