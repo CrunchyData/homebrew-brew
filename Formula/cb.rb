279dade734ee47df092b085e315397b78a8323f4
Adam Brightwell <adam.brightwell@crunchydata.com>
2024-11-05 17:05:09 -0500

    v3.6.1
    Closes #29.
    
    Signed-off-by: Adam Brightwell <adam.brightwell@gmail.com>

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -1,27 +1,27 @@
 class Cb < Formula
   desc "CLI for Crunchy Bridge"
   homepage "https://crunchybridge.com"
-  version "3.6.0"
+  version "3.6.1"
   license "GPL-3.0-or-later"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_macos_arm64.zip"
-    sha256 "42f62809bb76232ad098bfa3d817259270830fd23fde8457f4cb6efb8a2564a3"
+    sha256 "b0c52dbe6cc9ddcaf2c278bac2849e93f1e48f7bde575eb7e831a35fe8ff7848"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_macos_amd64.zip"
-    sha256 "6fea519db4d9a0e6f432f4ee52da96c22777eaacb67f034eaa7bd4b1c98591e3"
+    sha256 "3ffaaeac54d32b33b25c1f89996cbdde8cc81fe6dfc35737f960b648f62397a5"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_linux_aarch64.zip"
-    sha256 "66d3ec655b6d6ff7bbefc545be343d7ea731138bbc1e9cbe57e1e1d105aaea7a"
+    sha256 "a4513c5fee5de1d2e2aad373bd45243f2ae783eee5f708b8c9492fe955c163c2"
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_linux_amd64.zip"
-    sha256 "c5b90af19ebb91cd7f209c0ab67a749c15468df7568709873767a79fc5f42900"
+    sha256 "1c3bfcbdb09228b4b2fe55dfc52caf0fc5b517da763f2ea2fd51f4ba402ff92c"
   end
 
   def install
