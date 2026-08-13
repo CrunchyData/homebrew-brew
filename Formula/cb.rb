4a7cd7f4f6454ca613ba839986e95a2a73aa2c93
Erik Jones <erik.jones@snowflake.com>
2026-08-13 10:23:20 -0700

    Bump version and sha hashes for 3.7.1

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -1,27 +1,27 @@
 class Cb < Formula
   desc "CLI for Crunchy Bridge"
   homepage "https://crunchybridge.com"
-  version "3.6.7"
+  version "3.7.1"
   license "GPL-3.0-or-later"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_macos_arm64.zip"
-    sha256 "9be8e53e9f679e877384d2708ff5658698e01d844ba1a274008fb3a853b757b7"
+    sha256 "76b55b9556ff9586b8bb06e9471392cc81cf28468b9046e98e5b39da2fc21444"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_macos_amd64.zip"
-    sha256 "aae4f0169f56eae77cfa8ccc2b9db1b1bbe214399c05cc6caec491d5fd0f2fa5"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_linux_aarch64.zip"
-    sha256 "1a90f4c4c273959c681e0cb8a1191ca7c86b460d5e3ae2658f3ef5164200f2aa"
+    sha256 "d22d88a38af5ad077df37d09e5a8f552d602dc2d23aa147ad5dbcc8cb1b237c4"
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
     url "https://github.com/CrunchyData/bridge-cli/releases/download/v#{version}/cb-v#{version}_linux_amd64.zip"
-    sha256 "55df54a13761c3a3bb36f1bfc7a5d187cb79ea8aff33b621afa423a5cefe2ce9"
+    sha256 "57fe32a6d1b066c08fd7df5868d9010d5714a9b3784208b13ac853cd80774e3d"
   end
 
   def install
